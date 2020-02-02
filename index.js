106452de890ee20e0747f148e5cea8a6c460dd35
Beenish <55186839+beenish-mha@users.noreply.github.com>
2020-02-02 21:54:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Beemysql4", 
+    password: "", 
     database: "employee_tracker",
   });
 
